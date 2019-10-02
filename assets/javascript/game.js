6f2ca838efa1e82ff28cc2fd8fa9f54659bd6243
Kevin-eh <jollumpy@gmail.com>
2019-10-02 10:53:37 -0700

    creating a new dashes index

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,8 @@
+// generate random word
+// store that as "finalwordchoice"
+// create an array out of that word
+// create function that takes in a word and outputs an array of equal length
+
 var wordchoices = [
   "slake",
   "luxuriant",
@@ -7,9 +12,44 @@ var wordchoices = [
   "scrumptious"
 ];
 
+
 var finalwordchoice =
   wordchoices[Math.floor(Math.random() * wordchoices.length)];
+console.log(finalwordchoice);
+
+var targetword = finalwordchoice.split("")
+var dashing = []
+
 
 var finalwordtext = document.getElementById("finalwordchoice-text");
 
-finalwordtext.textContent = "the word is " + finalwordchoice;
+function underscores(targetword) {
+  for (var k = 0; k < targetword.length; k++) {
+    dashing.push("_")
+  }
+}
+
+// for (var j = 0; j < finalwordchoice.length; j++) {
+//   finalwordchoice[j] = "_";
+//   finalworddash = finalwordchoice[j];
+// }
+underscores(targetword)
+finalwordtext.textContent = "the word is " + dashing.join(" ");
+
+// var finalworddash = new char[finalwordchoice.length()]();
+//take in user input with an event listener 
+//if you have something you need to do more than once make it a function
+//take in user input
+//compare that to our targetword using a for loop
+
+function checktarget(letter) { }
+document.onkeyup = function (event) {
+  checktarget(event.key)
+}
+
+
+//take in the event key
+//using for loop, compare that to each index in our targetword array
+//if there is a match, replace index with corresponding letter
+//if no match at the end of the loop, decrease score
+//if the letter has already been guessed, do nothing
